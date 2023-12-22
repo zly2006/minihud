3bd683d4f3f412724dc39c12de8ad59a105e633c
Sakura Ryoko <116967773+sakura-ryoko@users.noreply.github.com>
2023-12-22 14:08:52 -0500

    23w51b-SNAP  0.31.0-snap

--- src/main/java/fi/dy/masa/minihud/renderer/BaseBlockRangeOverlay.java
+++ src/main/java/fi/dy/masa/minihud/renderer/BaseBlockRangeOverlay.java
@@ -1,5 +1,6 @@
 package fi.dy.masa.minihud.renderer;
 
+import fi.dy.masa.malilib.config.IConfigBoolean;
 import it.unimi.dsi.fastutil.longs.LongIterator;
 import it.unimi.dsi.fastutil.longs.LongOpenHashSet;
 import net.minecraft.block.entity.BlockEntity;
@@ -10,14 +11,13 @@ import net.minecraft.client.world.ClientChunkManager;
 import net.minecraft.client.world.ClientWorld;
 import net.minecraft.entity.Entity;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.ChunkSectionPos;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
 import net.minecraft.world.chunk.ChunkStatus;
 import net.minecraft.world.chunk.WorldChunk;
-import fi.dy.masa.malilib.config.IConfigBoolean;
 
-public abstract class BaseBlockRangeOverlay<T extends BlockEntity> extends OverlayRendererBase
-{
+public abstract class BaseBlockRangeOverlay<T extends BlockEntity> extends OverlayRendererBase {
     protected final IConfigBoolean renderToggleConfig;
     protected final LongOpenHashSet blockPositions = new LongOpenHashSet();
     protected final BlockEntityType<T> blockEntityType;
@@ -27,26 +27,21 @@ public abstract class BaseBlockRangeOverlay<T extends BlockEntity> extends Overl
     protected int updateDistance = 48;
 
     protected BaseBlockRangeOverlay(IConfigBoolean renderToggleConfig,
-                                    BlockEntityType<T> blockEntityType,
-                                    Class<T> blockEntityClass)
-    {
+            BlockEntityType<T> blockEntityType,
+            Class<T> blockEntityClass) {
         this.renderToggleConfig = renderToggleConfig;
         this.blockEntityType = blockEntityType;
         this.blockEntityClass = blockEntityClass;
     }
 
-    public void clear()
-    {
-        synchronized (this.blockPositions)
-        {
+    public void clear() {
+        synchronized (this.blockPositions) {
             this.blockPositions.clear();
         }
     }
 
-    public void setNeedsUpdate()
-    {
-        if (this.renderToggleConfig.getBooleanValue() == false)
-        {
+    public void setNeedsUpdate() {
+        if (this.renderToggleConfig.getBooleanValue() == false) {
             this.clear();
             return;
         }
@@ -55,12 +50,9 @@ public abstract class BaseBlockRangeOverlay<T extends BlockEntity> extends Overl
         this.needsFullRebuild = true;
     }
 
-    public void onBlockStatusChange(BlockPos pos)
-    {
-        if (this.renderToggleConfig.getBooleanValue())
-        {
-            synchronized (this.blockPositions)
-            {
+    public void onBlockStatusChange(BlockPos pos) {
+        if (this.renderToggleConfig.getBooleanValue()) {
+            synchronized (this.blockPositions) {
                 this.blockPositions.add(pos.asLong());
                 this.needsUpdate = true;
             }
@@ -68,28 +60,23 @@ public abstract class BaseBlockRangeOverlay<T extends BlockEntity> extends Overl
     }
 
     @Override
-    public boolean shouldRender(MinecraftClient mc)
-    {
+    public boolean shouldRender(MinecraftClient mc) {
         return this.renderToggleConfig.getBooleanValue();
     }
 
     @Override
-    public boolean needsUpdate(Entity cameraEntity, MinecraftClient mc)
-    {
+    public boolean needsUpdate(Entity cameraEntity, MinecraftClient mc) {
         return this.needsUpdate || this.lastUpdatePos == null ||
-               Math.abs(cameraEntity.getX() - this.lastUpdatePos.getX()) > this.updateDistance ||
-               Math.abs(cameraEntity.getZ() - this.lastUpdatePos.getZ()) > this.updateDistance;
+                Math.abs(cameraEntity.getX() - this.lastUpdatePos.getX()) > this.updateDistance ||
+                Math.abs(cameraEntity.getZ() - this.lastUpdatePos.getZ()) > this.updateDistance;
     }
 
     @Override
-    public void update(Vec3d cameraPos, Entity entity, MinecraftClient mc)
-    {
+    public void update(Vec3d cameraPos, Entity entity, MinecraftClient mc) {
         this.startBuffers();
 
-        synchronized (this.blockPositions)
-        {
-            if (this.needsFullRebuild)
-            {
+        synchronized (this.blockPositions) {
+            if (this.needsFullRebuild) {
                 this.blockPositions.clear();
                 this.fetchAllTargetBlockEntityPositions(mc.world, entity.getBlockPos(), mc);
                 this.needsFullRebuild = false;
@@ -103,37 +90,29 @@ public abstract class BaseBlockRangeOverlay<T extends BlockEntity> extends Overl
         this.needsUpdate = false;
     }
 
-    protected void startBuffers()
-    {
+    protected void startBuffers() {
         BUFFER_1.begin(this.renderObjects.get(0).getGlMode(), VertexFormats.POSITION_COLOR);
         BUFFER_2.begin(this.renderObjects.get(1).getGlMode(), VertexFormats.POSITION_COLOR);
     }
 
-    protected void uploadBuffers()
-    {
+    protected void uploadBuffers() {
         this.renderObjects.get(0).uploadData(BUFFER_1);
         this.renderObjects.get(1).uploadData(BUFFER_2);
     }
 
-    protected void fetchAllTargetBlockEntityPositions(ClientWorld world, BlockPos centerPos, MinecraftClient mc)
-    {
+    protected void fetchAllTargetBlockEntityPositions(ClientWorld world, BlockPos centerPos, MinecraftClient mc) {
         ClientChunkManager chunkManager = world.getChunkManager();
         int centerCX = centerPos.getX() >> 4;
         int centerCZ = centerPos.getZ() >> 4;
         int chunkRadius = mc.options.getViewDistance().getValue();
 
-        for (int cz = centerCZ - chunkRadius; cz <= centerCZ + chunkRadius; ++cz)
-        {
-            for (int cx = centerCX - chunkRadius; cx <= centerCX + chunkRadius; ++cx)
-            {
+        for (int cz = centerCZ - chunkRadius; cz <= centerCZ + chunkRadius; ++cz) {
+            for (int cx = centerCX - chunkRadius; cx <= centerCX + chunkRadius; ++cx) {
                 WorldChunk chunk = chunkManager.getChunk(cx, cz, ChunkStatus.FULL, false);
 
-                if (chunk != null)
-                {
-                    for (BlockEntity be : chunk.getBlockEntities().values())
-                    {
-                        if (be.getType() == this.blockEntityType)
-                        {
+                if (chunk != null) {
+                    for (BlockEntity be : chunk.getBlockEntities().values()) {
+                        if (be.getType() == this.blockEntityType) {
                             this.blockPositions.add(be.getPos().asLong());
                         }
                     }
@@ -142,29 +121,25 @@ public abstract class BaseBlockRangeOverlay<T extends BlockEntity> extends Overl
         }
     }
 
-    protected void renderBlockRanges(World world, Vec3d cameraPos, MinecraftClient mc)
-    {
+    protected void renderBlockRanges(World world, Vec3d cameraPos, MinecraftClient mc) {
         LongIterator it = this.blockPositions.iterator();
         BlockPos.Mutable mutablePos = new BlockPos.Mutable();
         double max = (mc.options.getViewDistance().getValue() + 2) * 16;
         max = max * max;
 
-        while (it.hasNext())
-        {
+        while (it.hasNext()) {
             mutablePos.set(it.nextLong());
             BlockEntity be = world.getBlockEntity(mutablePos);
 
-            if (be == null || this.blockEntityClass.isAssignableFrom(be.getClass()) == false)
-            {
+            if (be == null || this.blockEntityClass.isAssignableFrom(be.getClass()) == false) {
                 it.remove();
                 continue;
             }
 
             double distSq = (cameraPos.x - mutablePos.getX()) * (cameraPos.x - mutablePos.getX()) +
-                            (cameraPos.z - mutablePos.getZ()) * (cameraPos.z - mutablePos.getZ());
+                    (cameraPos.z - mutablePos.getZ()) * (cameraPos.z - mutablePos.getZ());
 
-            if (distSq > max)
-            {
+            if (distSq > max) {
                 continue;
             }
 
@@ -173,8 +148,7 @@ public abstract class BaseBlockRangeOverlay<T extends BlockEntity> extends Overl
         }
     }
 
-    protected int getTopYOverTerrain(World world, BlockPos pos, int range)
-    {
+    protected int getTopYOverTerrain(World world, BlockPos pos, int range) {
         final int minX = pos.getX() - range;
         final int minZ = pos.getZ() - range;
         final int maxX = pos.getX() + range;
@@ -186,15 +160,15 @@ public abstract class BaseBlockRangeOverlay<T extends BlockEntity> extends Overl
         final int maxCZ = maxZ >> 4;
         int maxY = 0;
 
-        for (int cz = minCZ; cz <= maxCZ; ++cz)
-        {
-            for (int cx = minCX; cx <= maxCX; ++cx)
-            {
+        for (int cz = minCZ; cz <= maxCZ; ++cz) {
+            for (int cx = minCX; cx <= maxCX; ++cx) {
                 WorldChunk chunk = world.getChunk(cx, cz);
-                int height = chunk.getHighestNonEmptySectionYOffset() + 15;
+                int yMaxMath = chunk.getHighestNonEmptySection(); // Replaces getHighestNonEmptySectionYOffset()
+                yMaxMath = yMaxMath == -1 ? chunk.getBottomY()
+                        : ChunkSectionPos.getBlockCoord(chunk.sectionIndexToCoord(yMaxMath));
+                int height = yMaxMath + 15;
 
-                if (height > maxY)
-                {
+                if (height > maxY) {
                     maxY = height;
                 }
             }
