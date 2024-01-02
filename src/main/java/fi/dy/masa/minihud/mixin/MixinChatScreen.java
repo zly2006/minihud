2aa23326ee30a86691abf2758b72eb5eb07e7054
Sakura Ryoko <116967773+sakura-ryoko@users.noreply.github.com>
2024-01-02 05:04:09 -0500

    1.20.x revert

--- src/main/java/fi/dy/masa/minihud/mixin/MixinChatScreen.java
+++ src/main/java/fi/dy/masa/minihud/mixin/MixinChatScreen.java
@@ -3,23 +3,26 @@ package fi.dy.masa.minihud.mixin;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-
-import fi.dy.masa.minihud.util.DataStorage;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import net.minecraft.client.gui.screen.ChatScreen;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.text.Text;
+import fi.dy.masa.minihud.util.DataStorage;
 
 @Mixin(ChatScreen.class)
-public abstract class MixinChatScreen extends Screen {
-    private MixinChatScreen(Text title) {
+public abstract class MixinChatScreen extends Screen
+{
+    private MixinChatScreen(Text title)
+    {
         super(title);
     }
 
     @Inject(method = "sendMessage", at = @At("HEAD"), cancellable = true)
-    private void onSendChatMessage(String msg, boolean addToHistory, CallbackInfo ci) {
-        if (DataStorage.getInstance().onSendChatMessage(msg)) {
-            ci.cancel();
+    private void onSendChatMessage(String msg, boolean addToHistory, CallbackInfoReturnable<Boolean> cir)
+    {
+        if (DataStorage.getInstance().onSendChatMessage(msg))
+        {
+            cir.setReturnValue(true);
         }
     }
 }
