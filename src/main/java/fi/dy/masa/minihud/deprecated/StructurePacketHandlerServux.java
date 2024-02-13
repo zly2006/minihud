64570dbfb943d3504f32e7eda46e2a97d1d9efe6
Sakura Ryoko <116967773+sakura-ryoko@users.noreply.github.com>
2024-02-12 23:19:06 -0500

    24w06a - Restore of 2 deprecated classes for historical reference.

---
+++ src/main/java/fi/dy/masa/minihud/deprecated/StructurePacketHandlerServux.java
@@ -0,0 +1,81 @@
+package fi.dy.masa.minihud.deprecated;
+
+//import net.minecraft.nbt.NbtCompound;
+//import net.minecraft.nbt.NbtList;
+//import net.minecraft.network.PacketByteBuf;
+//import net.minecraft.util.Identifier;
+
+//import fi.dy.masa.malilib.network.IPluginChannelHandler;
+//import fi.dy.masa.malilib.util.Constants;
+//import fi.dy.masa.minihud.MiniHUD;
+//import fi.dy.masa.minihud.data.DataStorage;
+
+/**
+ * Original Handler method, PROTOCOL_VERSION 1 and superseded by PROTOCOL_VERSION 2.
+ * As we cannot send a PacketByteBuf directly, but we can "cast" a dummy class to it, (Like I did for Syncmatica)
+ * but why bother when it unwraps the NbtCompound tag anyway?
+ */
+@Deprecated(forRemoval = false)
+//public class StructurePacketHandlerServux implements IPluginChannelHandler
+public class StructurePacketHandlerServux
+{
+    /*
+    public static final int PROTOCOL_VERSION = 1;
+    public static final int PACKET_S2C_METADATA = 1;
+    public static final int PACKET_S2C_STRUCTURE_DATA = 2;
+
+    public static final StructurePacketHandlerServux INSTANCE = new StructurePacketHandlerServux();
+
+    // Change Channel Identifier to having two parameters.  See ServUX.
+    private static final Identifier CHANNEL = new Identifier("servux", "structures");
+    private boolean registered;
+    private int timeout;
+
+    public void reset()
+    {
+        this.registered = false;
+    }
+
+    //@Override
+    public Identifier getChannel()
+    {
+        return CHANNEL;
+    }
+
+    //@Override
+    public void onPacketReceived(PacketByteBuf buf)
+    {
+        int id = buf.readVarInt();
+
+        MiniHUD.printDebug("StructurePacketHandlerServux#onPacketReceived(): type: {} (old timeout: {}, old reg: {})",
+                           id, this.timeout, this.registered);
+
+        if (id == PACKET_S2C_STRUCTURE_DATA && this.registered)
+        {
+            NbtCompound tag = buf.readNbt();
+
+            if (tag != null)
+            {
+                NbtList structures = tag.getList("Structures", Constants.NBT.TAG_COMPOUND);
+                MiniHUD.printDebug("StructurePacketHandlerServux#onPacketReceived(): structures; list size: {}", structures.size());
+                DataStorage.getInstance().addOrUpdateStructuresFromServer(structures, this.timeout, true);
+            }
+        }
+        else if (id == PACKET_S2C_METADATA)
+        {
+            NbtCompound tag = buf.readNbt();
+
+            if (tag != null &&
+                tag.getInt("version") == PROTOCOL_VERSION &&
+                tag.getString("id").equals(CHANNEL.toString()))
+            // Might need to remove the "id" string
+            {
+                this.timeout = tag.getInt("timeout");
+                this.registered = true;
+                DataStorage.getInstance().setIsServuxServer();
+                MiniHUD.printDebug("StructurePacketHandlerServux#onPacketReceived(): register; timeout: {}", this.timeout);
+            }
+        }
+    }
+     */
+}
