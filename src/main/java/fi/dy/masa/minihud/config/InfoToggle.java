a8b94662ab5c76c86ce9ab26f32e48abca3ab4f3
Liyan Zhao <return65535@qq.com>
2024-07-16 11:15:39 +0800

    fix

--- src/main/java/fi/dy/masa/minihud/config/InfoToggle.java
+++ src/main/java/fi/dy/masa/minihud/config/InfoToggle.java
@@ -63,7 +63,7 @@ public enum InfoToggle implements IConfigInteger, IHotkeyTogglable
     TIME_REAL               ("infoTimeIRL",                 true,   1, "", "minihud.config.info_toggle.comment.infoTimeIRL", "minihud.config.info_toggle.name.infoTimeIRL"),
     TIME_TOTAL_MODULO       ("infoTimeTotalModulo",         false, 34, "", "minihud.config.info_toggle.comment.infoTimeTotalModulo", "minihud.config.info_toggle.name.infoTimeTotalModulo"),
     TIME_WORLD              ("infoTimeWorld",               false,  2, "", "minihud.config.info_toggle.comment.infoTimeWorld", "minihud.config.info_toggle.name.infoTimeWorld"),
-    TIME_WORLD_FORMATTED    ("infoWorldTimeFormatted",      false,  3, "", "minihud.config.info_toggle.comment.infoWorldTimeFormatted", "minihud.config.info_toggle.name.infoWorldTimeFormatted")
+    TIME_WORLD_FORMATTED    ("infoWorldTimeFormatted",      false,  3, "", "minihud.config.info_toggle.comment.infoWorldTimeFormatted", "minihud.config.info_toggle.name.infoWorldTimeFormatted"),
     SERVUX                  ("infoServux",                  false,  9, "", "Show the current version of the Servux mod");
 
     public static final ImmutableList<InfoToggle> VALUES = ImmutableList.copyOf(values());
