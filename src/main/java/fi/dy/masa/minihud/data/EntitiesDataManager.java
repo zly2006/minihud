bd763d2f8d751cd3f211e20d16a324e23fa8a9f9
Sakura Ryoko <sakura@ryoko.soy>
2024-11-19 00:33:30 -0500

    update config for 1.21

--- src/main/java/fi/dy/masa/minihud/data/EntitiesDataManager.java
+++ src/main/java/fi/dy/masa/minihud/data/EntitiesDataManager.java
@@ -213,7 +213,7 @@ public class EntitiesDataManager implements IClientTickHandler
 
     private long getCacheTimeout()
     {
-        return (long) (MathHelper.clamp(Configs.Generic.ENTITY_DATA_SYNC_CACHE_TIMEOUT.getFloatValue(), 0.25f, 25.0f) * 1000L);
+        return (long) (MathHelper.clamp(Configs.Generic.ENTITY_DATA_SYNC_CACHE_TIMEOUT.getDoubleValue(), 0.25f, 25.0f) * 1000L);
     }
 
     private void tickCache()
